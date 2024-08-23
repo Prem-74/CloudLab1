f5c3085010cb6460fe9d9926ac4cea4956edf369
Prem-74 <131350725+Prem-74@users.noreply.github.com>
2024-08-23 11:32:43 +0530

    Update test_lab2_LinearR.py

--- test_lab2_LinearR.py
+++ test_lab2_LinearR.py
@@ -2,13 +2,13 @@ import pytest
 from Lab2_LinearR import train_and_predict
 
 def test_train_and_predict():
-    height = [[4.0], [5.0], [6.0], [7.0], [8.0], [9.0], [10.0]]
-    weight = [16, 25, 36, 49, 64, 81, 100]
+    height = [[1.0], [2.0], [3.0],[4.0], [5.0], [6.0], [7.0], [8.0], [9.0], [10.0]]
+    weight = [2,4,6,8,10,12,14,16,18,20]
     
     predicted_weight = train_and_predict(height, weight, 12.0)
     
-    expected_min = 110.0
-    expected_max = 146.0
+    expected_min = 10
+    expected_max = 14
     
     assert expected_min <= predicted_weight[0] <= expected_max, (
         f"Expected weight to be between {expected_min} and {expected_max}, "
